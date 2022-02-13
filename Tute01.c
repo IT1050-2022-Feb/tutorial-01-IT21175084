a57047b272747294b791e5491f30a7c63bbb3569
IT21175084 <99589093+IT21175084@users.noreply.github.com>
2022-02-13 22:29:00 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,23 @@
 #include <stdio.h>
 
 int main() {
+   int mark1,mark2, sum;
+   float avg;
+   
+   printf("Input 1st subject mark :%d");
+   scanf("%d",&mark1);
+   
+   printf("Input 2nd subject mark :%d");
+   scanf("%d",&mark2);
+   
+   sum=mark1+mark2;
+   printf("Sum of the two mark is :"sum,mark1,mark2);
+   
+   avg=(mark1+mark2)/2;
+   printf("avarage of the two numbers is :"avg,mark1,mark2);
+   
+   
+   
   
   return 0;
 }
